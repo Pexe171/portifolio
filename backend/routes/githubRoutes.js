a6e697dc2dd1222e8faa76f1919788581ba776c7
Pexe <davidhenriqusms18@gmail.com>
2025-10-29 23:44:56 -0400

    Zerar repositório e preservar README

--- backend/routes/githubRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { listarRepositorios } from '../controllers/githubController.js';
-
-const router = Router();
-
-router.get('/repos/:username', listarRepositorios);
-
-export default router;
